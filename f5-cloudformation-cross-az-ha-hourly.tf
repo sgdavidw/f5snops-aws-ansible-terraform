d5d769746b9aad9895572bc0c69bfe9e26400b2a
Tony Marfil <tonymarfil@gmail.com>
2017-08-02 20:38:24 -0500

    c4.xlarge

--- f5-cloudformation-cross-az-ha-hourly.tf
+++ f5-cloudformation-cross-az-ha-hourly.tf
@@ -16,7 +16,7 @@ resource "aws_cloudformation_stack" "f5-cluster-cross-az-ha-hourly" {
     #INSTANCE CONFIGURATION
 
     imageName            = "Good25Mbps"
-    instanceType         = "m4.large"
+    instanceType         = "c4.xlarge"
     restrictedSrcAddress = "0.0.0.0/0"
     sshKey               = "${var.aws_keypair}"
     restrictedSrcAddress = "0.0.0.0/0"
