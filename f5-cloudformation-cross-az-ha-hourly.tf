b7118d86195b39ba8108ded53b440e91b698b25d
Tony Marfil <marfil@olympus.f5net.com>
2018-05-27 00:07:22 -0400

    license from Big-IQ using v3.1.0 templates

--- f5-cloudformation-cross-az-ha-hourly.tf
+++ f5-cloudformation-cross-az-ha-hourly.tf
@@ -8,18 +8,18 @@ resource "aws_cloudformation_stack" "f5-cluster-cross-az-ha-hourly" {
     Vpc                          = "${aws_vpc.terraform-vpc.id}"
     managementSubnetAz1          = "${aws_subnet.f5-management-a.id}"
     managementSubnetAz2          = "${aws_subnet.f5-management-b.id}"
-    bigipManagementSecurityGroup = "${aws_security_group.f5_management.id}"
+    # bigipManagementSecurityGroup = "${aws_security_group.f5_management.id}"
     subnet1Az1                   = "${aws_subnet.public-a.id}"
     subnet1Az2                   = "${aws_subnet.public-b.id}"
-    bigipExternalSecurityGroup   = "${aws_security_group.f5_data.id}"
+    # bigipExternalSecurityGroup   = "${aws_security_group.f5_data.id}"
 
     #INSTANCE CONFIGURATION
 
     imageName            = "Good25Mbps"
-    instanceType         = "m4.xlarge"
+    instanceType         = "c5d.large"
     restrictedSrcAddress = "0.0.0.0/0"
     sshKey               = "${var.aws_keypair}"
-    restrictedSrcAddress = "0.0.0.0/0"
+    restrictedSrcAddressApp = "0.0.0.0/0"
     ntpServer            = "0.pool.ntp.org"
 
     #BIG-IQ LICENSING CONFIGURATION
@@ -41,5 +41,5 @@ resource "aws_cloudformation_stack" "f5-cluster-cross-az-ha-hourly" {
   }
 
   #CloudFormation templates triggered from Terraform must be hosted on AWS S3. Experimental hosted in non-canonical S3 bucket.
-  template_url = "https://s3.amazonaws.com/f5-public-cloud/f5-existing-stack-across-az-cluster-hourly-2nic-bigip.template"
+  template_url = "https://s3.amazonaws.com/f5-public-cloud/f5-existing-stack-across-az-cluster-hourly-2nic-bigip__v3.1.0_MODIFIED.template"
 }
